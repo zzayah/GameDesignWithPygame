09a1ee351d96ee9c7cd4ce61a5264b3eff7f6488
zzayah <zayahcortright@gmail.com>
2023-08-24 13:03:13 -0600

    yes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import math
 
+
 class Player:
 
     def __init__(self, name):
@@ -15,7 +16,7 @@ class Player:
         # list of die rolls
         die_rolls = []
         for p in range(6):
-            num = math.floor(random.random()*6 + 1)
+            num = math.floor(random.random() * 6 + 1)
             die_rolls.append(num)
         self.rolls = die_rolls
 
@@ -30,11 +31,17 @@ name_list = []
 gameboard = [1, 2, 3, 4, 5, 6]
 gameboardMod = ["O", "O", "O", "O", "O", "O"]
 current_space = 1
-found_match = True
+found_match = False
 start_turn_init = True
 
 # game conditions
 start = True
+
+
+# die
+def roll_die(self):
+    return [random.randomint(1, 6) for _ in range(6)]
+
 
 num_players = int(input("How many people are playing (1-4)?"))
 
@@ -48,32 +55,21 @@ print(name_list)
 while start:
     for player_index in range(len(name_list)):
         current_player = name_list[player_index]
-        current_player.roll_die()
-        all_rolls = current_player.get_rolls()
-        print(current_player)
-        while start_turn_init:
-            for i in range(6):
-                if current_space == all_rolls[i]:
-                    # remove, increment space, make player role a 0, reset loop to check
-                    current_player.remove_chips(1)
-                    current_space += 1
-                    all_rolls[i] = 0
+        input(f"{current_player.name}, press Enter to roll the die")
+        dice = roll_die().sort()
+
+        print(f"{current_player.name} rolled {dice}")
+
+        placed_die = []
+
+        for board_index in gameboard:
+            for roll_index in dice:
+                if dice[roll_index] == 1 and gameboardMod[0] == "O":
                     found_match = True
-                    gameboardMod[i] = "X"
-                    i = 0
-
-                    if gameboardMod == ["X", "X", "X", "X", "X", "X"]:
-                        print(f"Player {current_player} WON! Game finished.")
-                        start = False
-
-                    print(gameboard)
-                    print(gameboardMod)
-                    print(all_rolls)
-                    print(current_space)
-                    print(current_player)
-                    print(i)
-
-        if not found_match:
-            current_player.remove_chips(7-current_space)
-
+                    gameboardMod[board_index] = "X"
+                elif dice[roll_index] == gameboard[board_index] and board_index > 0 and gameboardMod[board_index-1] == "X":
+                    found_match = True
+                    gameboardMod[board_index] = "X"
+                else:
+                    break
 

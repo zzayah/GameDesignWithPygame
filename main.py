9c0f71f559ad3c8ea89f1c96fb252c9931fb222d
zzayah <zayahcortright@gmail.com>
2023-08-31 13:15:03 -0600

    8/31/23 in class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 pygame.init()
 DISPLAYSURF = pygame.display.set_mode((400, 300))
-trans_surf = DISPLAYSURF.convert_alpha()
+CHANGESURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
 
 # set up the colors
@@ -13,27 +13,25 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
-DISPLAYSURF.fill(BLUE)
+CHANGESURF.fill(BLUE)
 fpsClock = pygame.time.Clock()
 
 FPS = 30 # frames per second setting
 
-# Draw circles and ellipse
-# pygame.draw.circle(DISPLAYSURF, WHITE, (150, 100), 20)
-# pygame.draw.circle(DISPLAYSURF, WHITE, (250, 100), 20)
-
+eyebrow_surf = pygame.display.set_mode((400, 300))
+pygame.draw.rect(eyebrow_surf, RED, (0, 0, 50, 30))
 
 # Draw a transparent circle
 transparent_circle_surf = pygame.Surface((400, 300), pygame.SRCALPHA)
 pygame.draw.circle(transparent_circle_surf, (255, 255, 255, 128), (150, 100), 20)
 pygame.draw.circle(transparent_circle_surf, (255, 255, 255, 128), (250, 100), 20)
 
-DISPLAYSURF.blit(transparent_circle_surf, (0, 0))
+CHANGESURF.blit(transparent_circle_surf, (0, 0))
+CHANGESURF.blit(eyebrow_surf, (0, 0))
+
 
 play = True
-
 ellipseRad = 100
-ellipse = pygame.draw.ellipse(DISPLAYSURF, WHITE, (125, 150, 150, ellipseRad))
 direction = 'up'
 
 while True: # main game loop
@@ -50,16 +48,25 @@ while True: # main game loop
 
         if direction == 'up':
             ellipseRad += 5
-            if ellipseRad == 300:
+            if ellipseRad == 200:
                 direction = "down"
         elif direction == 'down':
             ellipseRad -= 5
-            if ellipseRad == 50:
+            if ellipseRad == 10:
                 direction = "up"
 
-        DISPLAYSURF.blit(ellipse, (0, 0))
+        # Create a surface to draw the ellipse
+        ellipse_surf = pygame.Surface((400, 300), pygame.SRCALPHA)
+        pygame.draw.ellipse(ellipse_surf, WHITE, (125, 150, 150, ellipseRad))
+
+        CHANGESURF.fill(BLUE)  # Clear the display
+        CHANGESURF.blit(eyebrow_surf, (0, 0))
+        CHANGESURF.blit(transparent_circle_surf, (0, 0)) # Blit the circles
+        CHANGESURF.blit(ellipse_surf, (0, 0))  # Blit the ellipse
 
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+
     pygame.display.update()
+    fpsClock.tick(FPS)

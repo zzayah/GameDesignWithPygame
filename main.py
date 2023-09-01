98f58e434f523acde6eaf70ec16d8d09609d31fb
zzayah <zayahcortright@gmail.com>
2023-09-01 12:58:06 -0600

    9/1/23 in class COMPLETE

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import pygame, sys
+import pygame
+import sys
+import time
 from pygame.locals import *
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+
+# Set up the screen
 CHANGESURF = pygame.display.set_mode((400, 300))
 pygame.display.set_caption('Hello World!')
 
-# set up the colors
+# Set up the colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
@@ -15,55 +18,53 @@ BLUE = (0, 0, 255)
 
 CHANGESURF.fill(BLUE)
 fpsClock = pygame.time.Clock()
+FPS = 30  # Frames per second setting
 
-FPS = 30 # frames per second setting
-
-eyebrow_surf = pygame.display.set_mode((400, 300))
-pygame.draw.rect(eyebrow_surf, RED, (0, 0, 50, 30))
+# eyebrow_surf = pygame.display.set_mode((400, 300))
+# pygame.draw.rect(eyebrow_surf, RED, (0, 0, 50, 30))
 
 # Draw a transparent circle
 transparent_circle_surf = pygame.Surface((400, 300), pygame.SRCALPHA)
-pygame.draw.circle(transparent_circle_surf, (255, 255, 255, 128), (150, 100), 20)
-pygame.draw.circle(transparent_circle_surf, (255, 255, 255, 128), (250, 100), 20)
+pygame.draw.circle(transparent_circle_surf, (0, 0, 0, 128), (150, 100), 20)
+pygame.draw.circle(transparent_circle_surf, (255, 0, 0, 128), (250, 100), 20)
 
 CHANGESURF.blit(transparent_circle_surf, (0, 0))
-CHANGESURF.blit(eyebrow_surf, (0, 0))
-
+# CHANGESURF.blit(eyebrow_surf, (0, 0))
 
 play = True
 ellipseRad = 100
 direction = 'up'
+fpsClock = pygame.time.Clock()
 
-while True: # main game loop
+while True:  # Main game loop
+
+    # Play sound once upon boot
+    while play:
+        soundObj = pygame.mixer.Sound('beep-02.mp3')
+        soundObj.play()
+        time.sleep(1)  # Wait and let the sound play for 1 second
+        soundObj.stop()
+        play = False
+
+    if direction == 'up':
+        ellipseRad += 5
+        if ellipseRad == 200:
+            direction = "down"
+    elif direction == 'down':
+        ellipseRad -= 5
+        if ellipseRad == 10:
+            direction = "up"
+
+    # Create a surface to draw the ellipse
+    ellipse_surf = pygame.Surface((400, 300), pygame.SRCALPHA)
+    pygame.draw.ellipse(ellipse_surf, WHITE, (125, 150, 150, ellipseRad))
+
+    CHANGESURF.fill(BLUE)  # Clear the display
+    # CHANGESURF.blit(eyebrow_surf, (0, 0))
+    CHANGESURF.blit(transparent_circle_surf, (0, 0))  # Blit the circles
+    CHANGESURF.blit(ellipse_surf, (0, 0))  # Blit the ellipse
+
     for event in pygame.event.get():
-
-        # Play sound once upon boot
-        while play:
-            soundObj = pygame.mixer.Sound('beep-02.mp3')
-            soundObj.play()
-            import time
-            time.sleep(1)  # wait and let the sound play for 1 second
-            soundObj.stop()
-            play = False
-
-        if direction == 'up':
-            ellipseRad += 5
-            if ellipseRad == 200:
-                direction = "down"
-        elif direction == 'down':
-            ellipseRad -= 5
-            if ellipseRad == 10:
-                direction = "up"
-
-        # Create a surface to draw the ellipse
-        ellipse_surf = pygame.Surface((400, 300), pygame.SRCALPHA)
-        pygame.draw.ellipse(ellipse_surf, WHITE, (125, 150, 150, ellipseRad))
-
-        CHANGESURF.fill(BLUE)  # Clear the display
-        CHANGESURF.blit(eyebrow_surf, (0, 0))
-        CHANGESURF.blit(transparent_circle_surf, (0, 0)) # Blit the circles
-        CHANGESURF.blit(ellipse_surf, (0, 0))  # Blit the ellipse
-
         if event.type == QUIT:
             pygame.quit()
             sys.exit()

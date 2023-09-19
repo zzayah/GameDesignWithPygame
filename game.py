6da8a0e67f6bab00f4ce4bfaffdafd87b2508a01
zzayah <zayahcortright@gmail.com>
2023-09-19 13:13:07 -0600

    9/19/23 in class

---
+++ game.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Game():
+    def __init__(self, board):
+        self.board = board
+        self.screenSize = screenSize
+        self.loadImages()
+
+    def run(self):
+        pygame.init()
+        screen = pygame.display.set_mode(self.screenSize)
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if(event.type == pygame.QUIT):
+                    running = False
+            self.draw()
+            pygame.display.flip()
+        pygame.quit()
+
+    def draw(self):
+        pass
+
+    def loadImages(self):
+
+
+

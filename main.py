369fa2a81a174ae5fcbaf2507693dc17d7e0e80d
zzayah <zayahcortright@gmail.com>
2023-09-28 13:13:06 -0600

    9/28/23 in class

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from game import Game
 from board import Board
-size = (9, 9)
+size = (18, 18)
 prob = 0.1
 board = Board(size, prob)
-screenSize = (board.getSize()[0]*16, board.getSize()[1]*16)
+screenSize = (board.getSize()[0]*16 + 100, board.getSize()[1]*16 + 100)
 game = Game(board, screenSize)
 game.run()

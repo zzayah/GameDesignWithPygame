91593097af5082da34f1e27ca76d7d43f60db03a
zzayah <107320284+zzayah@users.noreply.github.com>
2023-11-28 12:55:41 -0700

    asdf

---
+++ tile.py
@@ -0,0 +1,8 @@
+class Tile:
+
+    def __init__(self):
+        self.water = False
+        self.accelerate_left = False
+        self.accelerate_right = False
+        self.accelerate_up = False
+        self.accelerate_down = False

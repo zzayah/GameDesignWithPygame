5074ee076d392f8395efe1e83f1190dba9157030
zzayah <107320284+zzayah@users.noreply.github.com>
2023-12-16 19:57:44 -0700

    level 1 almost complete

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ class Main:
         self.lost = False
         self.won = False
 
+        self.sound_played = False
+
         self.alteration_right = 4
         self.alteration_down = 4
 
@@ -35,6 +37,7 @@ class Main:
             "info": (64, 480, 32, 32)
         }
 
+        self.time = None
         self.screen = pg.display.set_mode((776, 616))
 
     def set_board(self):
@@ -63,21 +66,62 @@ class Main:
                     self.board_tiles[row][col] = tile.Tile("floor")
 
     def draw(self):
-
         if self.lost:
+            self.screen.fill((0, 0, 0))
+            font = pg.font.Font(None, 70)
+            game_over_txt = font.render("GAME OVER", True, (255, 255, 255))
+            text_rect = game_over_txt.get_rect()
+            text_rect.center = (776//2, 616//2)
+            self.screen.blit(game_over_txt, text_rect)
+            pg.display.flip()
+        elif self.won:
             self.screen.fill((255, 255, 255))
             font = pg.font.Font(None, 70)
-            game_over_txt = font.render("GAME OVER", True, (0, 0, 0))
+            game_over_txt = font.render("YOU WIN!", True, (0, 0, 0))
             text_rect = game_over_txt.get_rect()
             text_rect.center = (776//2, 616//2)
             self.screen.blit(game_over_txt, text_rect)
             pg.display.flip()
         else:
-
             if self.first_turn:
                 # tiles are 64 x 64 (= 576) and y coordinate gives 20 pixels of room
                 self.screen.fill((255, 255, 255))
                 self.first_turn = False
+                font = pg.font.Font(None, 50)
+                time_txt = font.render("Time:", True, (0, 0, 0))
+                time_rect = time_txt.get_rect()
+                time_rect.center = (686, 100)
+                self.screen.blit(time_txt, time_rect)
+
+                chip_count_txt = font.render("Chips:", True, (0, 0, 0))
+                chip_rect = chip_count_txt.get_rect()
+                chip_rect.center = (686, 200)
+                self.screen.blit(chip_count_txt, chip_rect)
+
+            amt_chips = 0
+            for row in range(2):
+                for col in range(4):
+                    if self.inventory[row][col] == "chip":
+                        amt_chips += 1
+            
+            font = pg.font.Font(None, 50)
+            chip_ct_txt = font.render(f"{amt_chips}", True, (0, 0, 0))
+            chip_ct_rect = chip_ct_txt.get_rect()
+            chip_ct_rect.center = (686, 250)
+            chip_ct_white_rec = pg.Surface((50, 50))
+            chip_ct_white_rec.fill((255, 255, 255))
+            self.screen.blit(chip_ct_white_rec, chip_ct_rect)
+            self.screen.blit(chip_ct_txt, chip_ct_rect)
+
+            
+            # clock
+            clock_txt = font.render(f"{60-self.time//1000}", True, (0, 0, 0))
+            clock_rect = clock_txt.get_rect()
+            clock_rect.center = (686, 150)
+            white_rec = pg.Surface((50, 50))
+            white_rec.fill((255, 255, 255))
+            self.screen.blit(white_rec, clock_rect)
+            self.screen.blit(clock_txt, clock_rect)
 
             for row in range(2):
                 for col in range(4):
@@ -118,9 +162,31 @@ class Main:
 
     def run(self):
         pg.init()
+        pg.mixer.init()
         pg.display.set_caption("Chip's Challenge")
+        pg.mixer.music.load("background_msc.mp3")
+        pg.mixer.music.set_volume(.3)
+        pg.mixer.music.play(-1)
         self.set_board()  # Call set_board before entering the game loop
         while self.running:
+            self.time = pg.time.get_ticks()
+
+            if self.won:
+                if not self.sound_played:
+                    pg.mixer.music.stop()
+                    win_sound = pg.mixer.Sound("win.mp3")
+                    win_sound.set_volume(.3)
+                    win_sound.play()
+
+            if 60-(self.time//1000) < 0 or self.lost and not self.won:
+                self.lost = True
+                if not self.sound_played:
+                    pg.mixer.music.stop()
+                    lose_sound = pg.mixer.Sound("lose.mp3")
+                    lose_sound.set_volume(0.3)
+                    lose_sound.play()
+                    self.sound_played = True
+
             for event in pg.event.get():
                 if event.type == pg.QUIT:
                     self.running = False
@@ -129,7 +195,6 @@ class Main:
                         check_against = self.board_tiles[self.alteration_down-1][self.alteration_right].get_type()
                         if self.alteration_down > 4 and self.alteration_down <= len(self.board_tiles[0])-4:
                             if check_against == "solid":
-                                print("solid in the way")
                                 break
                             elif check_against == "wboot":
                                 self.board_tiles[self.alteration_down-1][self.alteration_right] = tile.Tile("floor")
@@ -170,10 +235,44 @@ class Main:
                         else:
                             print("self.alteration_down error. (pg.K_UP)")
                     elif event.key == pg.K_DOWN:
+                        check_against = self.board_tiles[self.alteration_down+1][self.alteration_right].get_type()
                         if self.alteration_down < len(self.board_tiles[0])-5:
-                            if self.board_tiles[self.alteration_down+1][self.alteration_right].get_type() == "solid":
-                                print("solid in the way")
+                            if check_against == "solid":
                                 break
+                            elif check_against == "wboot":
+                                self.board_tiles[self.alteration_down+1][self.alteration_right] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "wboot"
+                                        self.alteration_down += 1
+                                        break
+                            elif check_against == "chip":
+                                self.board_tiles[self.alteration_down+1][self.alteration_right] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "chip"
+                                        self.alteration_down += 1
+                                        break
+                            elif check_against == "red_k":
+                                self.board_tiles[self.alteration_down+1][self.alteration_right] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "red_k"
+                                        self.alteration_down += 1
+                                        break
+                            elif check_against == "red_d":
+                                for row in self.inventory:
+                                    if "red_k" in row:
+                                        row[row.index("red_k")] = "floor"
+                                        self.alteration_down += 1
+                                        self.board_tiles[self.alteration_down][self.alteration_right] = tile.Tile("floor")
+                                        break
+                            elif check_against == "water" and not (any("wboot" in row for row in self.inventory)):
+                                self.lost = True
+                                print("You are not Jesus. Next time, don't try to walk on water without Flippers. Game over.")
+                            elif check_against == "info":
+                                print("Level 1: Obtain the 4 chips located around the map by gathering the Red Key and Flippers and traversing through movement tiles.")
+                                self.alteration_down += 1
                             else:
                                 self.alteration_down += 1
                         else:
@@ -182,10 +281,45 @@ class Main:
                         check_against = self.board_tiles[self.alteration_down][self.alteration_right-1].get_type()
                         if self.alteration_right > 4 and self.alteration_right <= len(self.board_tiles)-4:
                             if check_against == "solid":
-                                print("solid in the way")
                                 break
+                            elif check_against == "wboot":
+                                self.board_tiles[self.alteration_down][self.alteration_right-1] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "wboot"
+                                        self.alteration_right -= 1
+                                        break
+                            elif check_against == "chip":
+                                self.board_tiles[self.alteration_down][self.alteration_right-1] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "chip"
+                                        self.alteration_right -= 1
+                                        break
+                            elif check_against == "red_k":
+                                self.board_tiles[self.alteration_down][self.alteration_right-1] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "red_k"
+                                        self.alteration_right -= 1
+                                        break
+                            elif check_against == "red_d":
+                                for row in self.inventory:
+                                    if "red_k" in row:
+                                        row[row.index("red_k")] = "floor"
+                                        self.alteration_right -= 1
+                                        self.board_tiles[self.alteration_down][self.alteration_right-1] = tile.Tile("floor")
+                                        break
+                            elif check_against == "water" and not (any("wboot" in row for row in self.inventory)):
+                                self.lost = True
+                                print("You are not Jesus. Next time, don't try to walk on water without Flippers. Game over.")
+                            elif check_against == "info":
+                                print("Level 1: Obtain the 4 chips located around the map by gathering the Red Key and Flippers and traversing through movement tiles.")
+                                self.alteration_right -= 1
                             elif check_against == "acc_le":
                                 self.alteration_right -= 2
+                                break
+                            elif check_against == "acc_ri":
                                 break
                             else:
                                 self.alteration_right -= 1
@@ -195,7 +329,42 @@ class Main:
                         check_against = self.board_tiles[self.alteration_down][self.alteration_right+1].get_type()
                         if self.alteration_right < len(self.board_tiles)-5:
                             if check_against == "solid":
-                                print("solid in the way")
+                                break
+                            elif check_against == "wboot":
+                                self.board_tiles[self.alteration_down][self.alteration_right+1] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "wboot"
+                                        self.alteration_right += 1
+                                        break
+                            elif check_against == "chip":
+                                self.board_tiles[self.alteration_down][self.alteration_right+1] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "chip"
+                                        self.alteration_right += 1
+                                        break
+                            elif check_against == "red_k":
+                                self.board_tiles[self.alteration_down][self.alteration_right+1] = tile.Tile("floor")
+                                for row in self.inventory:
+                                    if "floor" in row:
+                                        row[row.index("floor")] = "red_k"
+                                        self.alteration_right += 1
+                                        break
+                            elif check_against == "red_d":
+                                for row in self.inventory:
+                                    if "red_k" in row:
+                                        row[row.index("red_k")] = "floor"
+                                        self.alteration_right += 1
+                                        self.board_tiles[self.alteration_down][self.alteration_right+1] = tile.Tile("floor")
+                                        break
+                            elif check_against == "water" and not (any("wboot" in row for row in self.inventory)):
+                                self.lost = True
+                                print("You are not Jesus. Next time, don't try to walk on water. Game over.")
+                            elif check_against == "info":
+                                print("Level 1: Obtain the 4 chips located around the map by gathering the Red Key and Flippers and traversing through movement tiles.")
+                                self.alteration_right += 1
+                            elif check_against == "acc_le":
                                 break
                             elif check_against == "acc_ri":
                                 self.alteration_right += 2

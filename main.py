82d9f3e893d746d218a574eb94e4f8c12e2b2f13
zzayah <107320284+zzayah@users.noreply.github.com>
2024-01-11 13:15:15 -0700

    1/11/24

--- main.py
+++ main.py
@@ -2,39 +2,50 @@ import pygame as pg
 
 class Main:
     def __init__(self, board_size):
+        # board_size must be a tuple, representing the width (x) then height (y) of the board
         self.filename = "snake_sprites.png"
-        size_mapping = {
-            "small": (1, 1),
-            "medium": (17, 17),
-            "large": (25, 25)
-        }
-        self.board_size = size_mapping.get(board_size, (0, 0))
 
-        if self.board_size == (0, 0):
-            print("input error: board_size not recognized")
+        # green2 and green1 init
+        self.green2 = pg.image.load("green2.png")
+        self.green1 = pg.image.load("green1.png")
+
+        self.board_size = (board_size)
+
+        if not (10 < self.board_size[0] < 26):
+            print("invalid board_size input")
             pg.quit()
-        
-        # init green_board
-        for i in range(0, self.board_size[0]):
-            for j in range(0, self.board_size[1]):
-                break
 
-        self.screen = pg.display.set_mode((self.board_size[0]*64)+40, (self.board_size[1]*64)+40)
+        self.screen = pg.display.set_mode((((self.board_size[0]*64)+40), ((self.board_size[1]*64)+40)))
 
-    def set_board(self):
-        # fill screen with green3
-        self.screen.fill((87, 138, 52))
-
-        # blit 
-        for i in range(0, 17):
-            for j in range(0, 17):
-                break
+    def do_periodic(self):
+        # blit tiles
+        counter = 0
+        for i in range(0, self.board_size[1]):
+            for j in range(0, self.board_size[0]):
+                if not (self.board_size[0] % 2 == 0 or self.baord_size[1] % 2 == 0):
+                    if not (counter % 2 != 0):
+                        self.screen.blit(self.green2, ((j*64)+20, (i*64)+20))
+                    else:
+                        self.screen.blit(self.green1, ((j*64)+20, (i*64)+20))
+                else:
+                    if counter % 2 != 0:
+                        self.screen.blit(self.green2, ((j*64)+20, (i*64)+20))
+                    else:
+                        self.screen.blit(self.green1, ((j*64)+20, (i*64)+20))
+                counter += 1
     
     def run(self):
         pg.init()
+        pg.display.set_caption("Snake")
+        self.screen.fill((87, 138, 52))
         running = True
         while running:
             for event in pg.event.get():
-                if event.type() == pg.QUIT:
+                if event == pg.QUIT:
                     running = False
+            self.do_periodic()
+            pg.display.flip()
         pg.quit()
+
+main = Main((14, 14))
+main.run()

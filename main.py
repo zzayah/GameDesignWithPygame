00c1ee7faff0eb3eceee227ac7212a070968d3a2
zzayah <107320284+zzayah@users.noreply.github.com>
2024-01-17 20:08:27 -0700

    1/17/24

--- main.py
+++ main.py
@@ -5,9 +5,41 @@ class Main:
         # board_size must be a tuple, representing the width (x) then height (y) of the board
         self.filename = "snake_sprites.png"
 
+        self.snake_pos = [[0 for _ in range(board_size)] for _ in range(board_size)]
+
+        self.snake_pos[int(board_size/2)][1] = 1
+        self.snake_pos[int(board_size/2)][2] = 300
+        self.snake_pos[int(board_size/2)][3] = 10
+
         # green2 and green1 init
         self.green2 = pg.image.load("green2.png")
         self.green1 = pg.image.load("green1.png")
+
+        # snake sprites init
+        self.snake_sprites = pg.image.load("snake_sprites.png")
+
+        self.snake_sprite_pos = {
+            # all in (x, y)
+
+            # tail_east
+            1: (256, 128, 64, 64),
+            # tail_north
+            2: (192, 128, 64, 64),
+            # tail_west
+            3: (192, 192, 64, 64),
+            # tail_south
+            4: (256, 192, 64, 64),
+            # head_east
+            10: (256, 0, 64, 64),
+            # head_north
+            20: (192, 0, 64, 64),
+            # head_west
+            30: (192, 64, 64, 64),
+            # head_south
+            40: (256, 64, 64, 64),
+            # body_east_west
+            300: (64, 0, 64, 64)
+        }
 
         self.board_size = (board_size, board_size)
 
@@ -18,21 +50,22 @@ class Main:
         self.screen = pg.display.set_mode((((self.board_size[0]*64)+40), ((self.board_size[1]*64)+40)))
 
     def do_periodic(self):
-        # blit tiles
-        counter = 0
-        for i in range(0, self.board_size[1]):
-            for j in range(0, self.board_size[0]):
-                if not (self.board_size[0] % 2 == 0 or self.board_size[1] % 2 == 0):
-                    if not (counter % 2 != 0):
-                        self.screen.blit(self.green2, ((j*64)+20, (i*64)+20))
-                    else:
-                        self.screen.blit(self.green1, ((j*64)+20, (i*64)+20))
+        # Blit tiles in checkerboard pattern
+        for i in range(self.board_size[1]):
+            for j in range(self.board_size[0]):
+                if (i + j) % 2 == 0:
+                    self.screen.blit(self.green2, ((j*64)+20, (i*64)+20))
                 else:
-                    if counter % 2 != 0:
-                        self.screen.blit(self.green2, ((j*64)+20, (i*64)+20))
-                    else:
-                        self.screen.blit(self.green1, ((j*64)+20, (i*64)+20))
-                counter += 1
+                    self.screen.blit(self.green1, ((j*64)+20, (i*64)+20))
+        
+        for i in range(self.board_size[1]):
+            for j in range(self.board_size[0]):
+                if self.snake_pos[i][j] != 0:
+                    snake_rect = pg.Rect(self.snake_sprite_pos[self.snake_pos[i][j]])
+                    snake_piece = self.snake_sprites.subsurface(snake_rect)
+                    snake_piece.set_colorkey((255, 255, 255))
+                    self.screen.blit(snake_piece, ((j * 64 + 20), (i * 64 + 20)))
+
     
     def run(self):
         pg.init()
@@ -41,7 +74,7 @@ class Main:
         running = True
         while running:
             for event in pg.event.get():
-                if event == pg.QUIT:
+                if event == pg.QUIT or event == pg.K_ESCAPE:
                     running = False
             self.do_periodic()
             pg.display.flip()

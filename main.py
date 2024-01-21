50f5d26a221b6022657d54fb16aa2a9807f39b8c
zzayah <107320284+zzayah@users.noreply.github.com>
2024-01-20 19:14:52 -0700

    1/20/24

--- main.py
+++ main.py
@@ -7,39 +7,53 @@ class Main:
 
         self.snake_pos = [[0 for _ in range(board_size)] for _ in range(board_size)]
 
-        self.snake_pos[int(board_size/2)][1] = 1
-        self.snake_pos[int(board_size/2)][2] = 300
-        self.snake_pos[int(board_size/2)][3] = 10
+        self.snake_pos[int(board_size/2)][1] = "right"
+        self.snake_pos[int(board_size/2)][2] = "right"
+        self.snake_pos[int(board_size/2)][3] = "right"
+
+        # snake head location
+        self.snake_head = (int(board_size/2), 3)
 
         # green2 and green1 init
         self.green2 = pg.image.load("green2.png")
         self.green1 = pg.image.load("green1.png")
 
+        # red and blue init
+        self.red = pg.image.load("red.png")
+        self.blue = pg.image.load("blue.png")
+        self.gold = pg.image.load("gold.png")
+
         # snake sprites init
-        self.snake_sprites = pg.image.load("snake_sprites.png")
+        # self.snake_sprites = pg.image.load("snake_sprites.png")
 
-        self.snake_sprite_pos = {
-            # all in (x, y)
+        # clock var
+        self.old_time = 0
+        self.new_time = 0
 
-            # tail_east
-            1: (256, 128, 64, 64),
-            # tail_north
-            2: (192, 128, 64, 64),
-            # tail_west
-            3: (192, 192, 64, 64),
-            # tail_south
-            4: (256, 192, 64, 64),
-            # head_east
-            10: (256, 0, 64, 64),
-            # head_north
-            20: (192, 0, 64, 64),
-            # head_west
-            30: (192, 64, 64, 64),
-            # head_south
-            40: (256, 64, 64, 64),
-            # body_east_west
-            300: (64, 0, 64, 64)
-        }
+        # input direction
+        self.input_direction = "right"
+
+        # self.snake_sprite_pos = {
+        #     # all in (x, y)
+        #     # tail_east
+        #     1: (256, 128, 64, 64),
+        #     # tail_north
+        #     2: (192, 128, 64, 64),
+        #     # tail_west
+        #     3: (192, 192, 64, 64),
+        #     # tail_south
+        #     4: (256, 192, 64, 64),
+        #     # head_east
+        #     10: (256, 0, 64, 64),
+        #     # head_north
+        #     20: (192, 0, 64, 64),
+        #     # head_west
+        #     30: (192, 64, 64, 64),
+        #     # head_south
+        #     40: (256, 64, 64, 64),
+        #     # body_east_west
+        #     300: (64, 0, 64, 64)
+        # }
 
         self.board_size = (board_size, board_size)
 
@@ -61,25 +75,38 @@ class Main:
         for i in range(self.board_size[1]):
             for j in range(self.board_size[0]):
                 if self.snake_pos[i][j] != 0:
-                    snake_rect = pg.Rect(self.snake_sprite_pos[self.snake_pos[i][j]])
-                    snake_piece = self.snake_sprites.subsurface(snake_rect)
-                    snake_piece.set_colorkey((255, 255, 255))
-                    self.screen.blit(snake_piece, ((j * 64 + 20), (i * 64 + 20)))
+                    self.screen.blit(self.red, ((j * 64 + 20), (i * 64 + 20)))
+
+    def handle_input(self):
+        return
 
     
     def run(self):
         pg.init()
-        # init clock
-        Clock = pg.time.Clock
-        time = None
         pg.display.set_caption("Snake")
         self.screen.fill((87, 138, 52))
         running = True
         while running:
+            self.new_time = pg.time.get_ticks()
+
+            if self.new_time - 500 > self.old_time:
+                # abstract to function
+                self.handle_input()
+                # restart counter
+                self.old_time = self.new_time
+
             for event in pg.event.get():
                 if event == pg.QUIT or event == pg.K_ESCAPE:
                     running = False
-            
+                elif event == pg.K_UP:
+                    self.input_direction = "up"
+                elif event == pg.K_DOWN:
+                    self.input_direction = "down"
+                elif event == pg.K_LEFT:
+                    self.input_direction = "left"
+                elif event == pg.K_RIGHT:
+                    self.input_direction = "right"
+
             self.do_periodic()
             pg.display.flip()
         pg.quit()

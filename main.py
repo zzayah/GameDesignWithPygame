e5527c9dc674aa9d759452d0c05011ab129b627c
zzayah <107320284+zzayah@users.noreply.github.com>
2024-01-23 13:14:29 -0700

    1/23/24

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ class Main:
         self.filename = "snake_sprites.png"
 
         self.snake_pos = [[0 for _ in range(board_size)] for _ in range(board_size)]
+        self.marker_ary = [["" for _ in range(board_size)] for _ in range(board_size)]
 
         self.snake_pos[int(board_size/2)][1] = ((int(board_size/2), 1), "right")
         self.snake_pos[int(board_size/2)][2] = ((int(board_size/2), 2), "right")
@@ -30,6 +31,10 @@ class Main:
         self.old_time = 0
         self.new_time = 0
         
+        # lost/win var
+        self.lost = False
+        self.won = False
+
         # snake length
         self.snake_length = 3
 
@@ -80,48 +85,88 @@ class Main:
                 if self.snake_pos[i][j] != 0:
                     self.screen.blit(self.red, ((j * 64 + 20), (i * 64 + 20)))
 
-    def handle_input(self):
-        # move snake
-        if self.input_direction == "up" and self.snake_pos[self.snake_head[0]][self.snake_head[1] - 1] == 0:
-            self.snake_head = (self.snake_head[0], self.snake_head[1] - 1)
-            for i in range(self.snake_length):
-                for j in range(self.snake_length):
-                    if self.snake_pos[i][j] != 0:
-                        break
-        elif self.input_direction == "down" and self.snake_pos[self.snake_head[0]][self.snake_head[1] + 1] == 0:
-            self.snake_head = (self.snake_head[0], self.snake_head[1] + 1)
-        elif self.input_direction == "left" and self.snake_pos[self.snake_head[0] - 1][self.snake_head[1]] == 0:
-            self.snake_head = (self.snake_head[0] - 1, self.snake_head[1])
-        elif self.input_direction == "right" and self.snake_pos[self.snake_head[0] + 1][self.snake_head[1]] == 0:
-            self.snake_head = (self.snake_head[0] + 1, self.snake_head[1])
-        return
+    def handle_input(self, event):
 
-    
+        if event:
+        # move snake
+            if self.input_direction == "up" and self.snake_pos[self.snake_head[0]][self.snake_head[1] - 1] == 0:
+                self.snake_head = (self.snake_head[0], self.snake_head[1] - 1)
+                # update marker ary
+                self.marker_ary[self.snake_head[0]][self.snake_head[1]] = "up"
+            elif self.input_direction == "down" and self.snake_pos[self.snake_head[0]][self.snake_head[1] + 1] == 0:
+                self.snake_head = (self.snake_head[0], self.snake_head[1] + 1)
+            elif self.input_direction == "left" and self.snake_pos[self.snake_head[0] - 1][self.snake_head[1]] == 0:
+                self.snake_head = (self.snake_head[0] - 1, self.snake_head[1])
+            elif self.input_direction == "right" and self.snake_pos[self.snake_head[0] + 1][self.snake_head[1]] == 0:
+                self.snake_head = (self.snake_head[0] + 1, self.snake_head[1])
+            else:
+                self.lost = True
+            return
+        else:
+            # move all parts of the snake
+            for i in range(self.board_size[1]):
+                for j in range(self.board_size[0]):
+                    if self.snake_pos[i][j] != 0:
+                        # all marker_ary cases
+                        if self.marker_ary[i][j] == "right" and self.snake_pos[i][j][1] != "left":
+                            self.snake_pos[i][j+1] = ((self.snake_pos[i][j][0][0] + 1, self.snake_pos[i][j][0][1]), "right")
+                            self.snake_pos[i][j] = 0
+                        elif self.marker_ary[i][j] == "left" and self.snake_pos[i][j][1] != "right":
+                            self.snake_pos[i][j-1] = ((self.snake_pos[i][j][0][0] - 1, self.snake_pos[i][j][0][1]), "left")
+                            self.snake_pos[i][j] = 0
+                        elif self.marker_ary[i][j] == "down" and self.snake_pos[i][j][1] != "up":
+                            self.snake_pos[i-1][j] = ((self.snake_pos[i][j][0][0], self.snake_pos[i][j][0][1] + 1), "down")
+                            self.snake_pos[i][j] = 0
+                        elif self.marker_ary[i][j] == "up" and self.snake_pos[i][j][1] != "down":
+                            self.snake_pos[i+1][j] = ((self.snake_pos[i][j][0][0], self.snake_pos[i][j][0][1] - 1), "up")
+                            self.snake_pos[i][j] = 0
+                        else:
+                            # all snake_pos cases
+                            if self.marker_ary[i][j] == "right" and self.snake_pos[i][j][1] != "left" and j+1 < len(self.snake_pos[i]):
+                                self.snake_pos[i][j+1] = ((self.snake_pos[i][j][0][0] + 1, self.snake_pos[i][j][0][1]), "right")
+                                self.snake_pos[i][j] = 0
+                            elif self.snake_pos[i][j][1] == "left":
+                                self.snake_pos[i][j-1] = ((self.snake_pos[i][j][0][0] - 1, self.snake_pos[i][j][0][1]), "left")
+                                self.snake_pos[i][j] = 0
+                            elif self.snake_pos[i][j][1] == "down":
+                                self.snake_pos[i][j-1] = ((self.snake_pos[i][j][0][0], self.snake_pos[i][j][0][1] + 1), "down")
+                                self.snake_pos[i][j] = 0
+                            elif self.snake_pos[i][j][0] == "up":
+                                self.snake_pos[i][j+1] = ((self.snake_pos[i][j][0][0], self.snake_pos[i][j][0][1] - 1), "up")
+                                self.snake_pos[i][j] = 0
+
     def run(self):
         pg.init()
         pg.display.set_caption("Snake")
         self.screen.fill((87, 138, 52))
         running = True
         while running:
-            self.new_time = pg.time.get_ticks()
-
-            if self.new_time - 500 > self.old_time:
-                # abstract to function
-                self.handle_input()
-                # restart counter
-                self.old_time = self.new_time
-
+            event_occured = False
             for event in pg.event.get():
                 if event == pg.QUIT or event == pg.K_ESCAPE:
                     running = False
                 elif event == pg.K_UP:
                     self.input_direction = "up"
+                    event_occured = True
                 elif event == pg.K_DOWN:
                     self.input_direction = "down"
+                    event_occured = True
                 elif event == pg.K_LEFT:
                     self.input_direction = "left"
+                    event_occured = True
                 elif event == pg.K_RIGHT:
                     self.input_direction = "right"
+                    event_occured = True
+
+            self.new_time = pg.time.get_ticks()
+
+            if self.new_time - 500 > self.old_time and event_occured == True:
+                # abstract to function
+                self.handle_input(True)
+                # restart counter
+                self.old_time = self.new_time
+            elif self.new_time - 500 > self.old_time:
+                self.handle_input(False)
 
             self.do_periodic()
             pg.display.flip()
